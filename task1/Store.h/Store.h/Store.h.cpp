70e3ec36491461f90562caa00d1129b2a8767757
Черных Никита <nikitos89nur@gmail.com>
2025-02-21 09:16:11 +0300

    да

--- task1/Store.h/Store.h/Store.h.cpp
+++ task1/Store.h/Store.h/Store.h.cpp
@@ -1,5 +1,5 @@
 ﻿#ifndef ALL_CLASSES_H
-#define ALL_CLASSES_H
+#define ALL_CLASSES_H //Защищают от повторного включения заголовочного файла
 
 #include <string>
 #include <vector>
@@ -12,14 +12,14 @@ private:
     int quantity;
 
 public:
-    Product(std::string name, double price, int quantity);
+    Product(std::string name, double price, int quantity);//Конструктор для инициализации товара
 
     std::string getName() const;
     double getPrice() const;
-    int getQuantity() const;
+    int getQuantity() const;//Геттеры для получения значений атрибутов
 
     void setPrice(double price);
-    void setQuantity(int quantity);
+    void setQuantity(int quantity);//Методы для изменения цены и количества
 };
 
 // Класс Store (Магазин)
@@ -29,12 +29,12 @@ private:
     std::vector<Product> products;
 
 public:
-    Store(std::string name);
+    Store(std::string name);//Конструктор для создания магазина
 
-    std::string getName() const;
-    void addProduct(std::string productName, double price, int quantity);
-    double getPrice(std::string productName) const;
-    bool purchaseProduct(std::string productName, int quantity);
+    std::string getName() const;//Геттер для имени магазина
+    void addProduct(std::string productName, double price, int quantity);//Метод для добавления товаров
+    double getPrice(std::string productName) const;//Метод для получения цены товара по имени
+    bool purchaseProduct(std::string productName, int quantity);//Метод для покупки товара
 };
 
 // Класс Customer (Покупатель)
@@ -44,13 +44,13 @@ private:
     double balance;
 
 public:
-    Customer(std::string name, double balance);
+    Customer(std::string name, double balance);//Конструктор для инициализации покупателя
 
     std::string getName() const;
-    double getBalance() const;
+    double getBalance() const;//Геттеры для полученя имени и баланса
 
-    void addFunds(double amount);
-    void buyProduct(Store& store, std::string productName, int quantity);
+    void addFunds(double amount);//Метод для пополнения баланса
+    void buyProduct(Store& store, std::string productName, int quantity);//Метод для покупки товара в магазине
 };
 
 // Класс StoreManager (Управление магазинами)
@@ -59,8 +59,8 @@ private:
     std::vector<Store> stores;
 
 public:
-    void addStore(Store store);
-    Store* findCheapestStore(std::string productName);
+    void addStore(Store store);//Метод для добавления магазина в список
+    Store* findCheapestStore(std::string productName);//Метод для поиска магазина с самой низкой ценой на товар
 };
 
-#endif // ALL_CLASSES_H
+#endif // Завершает ifndef предотвращая дублирование кода
